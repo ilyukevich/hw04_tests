fa7f2d81a5b559722942c93555fab13e1312d9b2
ilyukevich <9009901@gmail.com>
2020-08-18 02:05:39 +0300

    17/08/2020 - review2

--- posts/tests.py
+++ posts/tests.py
@@ -1,52 +1,65 @@
-# from django.test import TestCase
-#
-#
-# class TestStringMethods(TestCase):
-#     def test_length(self):
-#                 self.assertEqual(len('yatube'), 6)
-#
-#     def test_show_msg(self):
-#                 # действительно ли первый аргумент — True?
-#                 self.assertTrue(False, msg="Важная проверка на истинность")
-
 from django.test import TestCase, Client
+from .models import Post, User
+from django.urls import reverse
 
-from .models import User
 
-
-class TestProfile(TestCase):
+class ProfileTest(TestCase):
     def setUp(self):
         self.client = Client()
         self.user = User.objects.create_user(
-            username="testuser",
-            email="testuser@testmail.by",
-            password="testpass",
+            username="sarah",
+            email="connor.s@skynet.com",
+            password="12345"
+        )
+        self.post = Post.objects.create(
+            text="test_text",
+            author=self.user
         )
 
-    def test_profile(self):
-        user_profile = self.client.get("/testuser/")
-        self.assertEqual(
-            user_profile.status_code,
-            200,
-            msg="Профиль пользователя после регистрации не создаётся",
-        )
-        self.assertTemplateUsed(user_profile, "profile.html")
-        self.assertContains(user_profile, "testuser")
 
-    def test_new_post(self):
-        self.client.login(username="testuser", password="testpass")
+    def test_personal_page(self):
+        """After registering a user, his personal page (profile) is created"""
+        profile = self.client.get("/sarah/")
+        self.assertEqual(profile.status_code, 200)
+        self.assertContains(profile, "sarah")
+
+
+    def test_create_post(self):
+        """test_create_post"""
+        # An authorized user can publish a post (new)
+        self.client.post('/auth/login/', {'username': 'sarah', 'password': '12345'})
         new_post = self.client.get("/new/", follow=True)
-        self.assertEqual(
-            new_post.status_code,
-            200,
-            msg="Авторизованный юзер не может создать новый пост",
-        )
-        self.assertTemplateUsed(new_post, "new_post.html")
+        self.assertEqual(new_post.status_code, 200)
+        self.assertTemplateUsed(new_post, "new.html")
+
+        # An unauthorized visitor cannot publish a post (it is redirected to the login page)
         self.client.logout()
-        index = self.client.get("/")
-        self.assertNotContains(index, "testuser")
-        self.assertRedirects(
-            new_post, "http://testserver/auth/login/?next=/new/"
-        )
-        self.assertTemplateNotUsed(new_post, "new_post.html")
-        self.assertTemplateUsed(new_post, "login.html")
+        response = self.client.get("/new/")
+        self.assertRedirects(response, '/auth/login/?next=/new/')
+
+
+    def test_publication(self):
+        """After the post is published, a new post appears on the main page of the site (index), on the user's
+        personal page (profile), and on a separate post page (post)"""
+        response = self.client.get("/")
+        self.assertContains(response, self.post.text)
+        response = self.client.get(reverse("profile", args=[self.user.username]))
+        self.assertContains(response, self.post.text)
+        response = self.client.get(reverse("post", args=[self.user.username, self.post.id]))
+        self.assertContains(response, self.post.text)
+
+
+    def test_edit(self):
+        """An authorized user can edit his post and its content will change on all linked pages"""
+        self.client.post('/auth/login/', {'username': 'sarah', 'password': '12345'})
+        self.client.post(reverse("post_edit", args=[self.user.username, self.post.id]), {"text": "GOOD"})
+        p = self.post.id
+        response = self.client.get("/")
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "GOOD")
+        response = self.client.get("/sarah/")
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "GOOD")
+        response = self.client.get("/sarah/1/")
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "GOOD")
--- posts/views.py
+++ posts/views.py
@@ -47,12 +47,9 @@ def new_post(request):
     return render(request, 'new.html', {'form': form})
 
 
+# close pages from unauthorized users
 @login_required
 def post_edit(request, username, post_id):
-    # тут тело функции. Не забудьте проверить,
-    # что текущий пользователь — это автор записи.
-    # В качестве шаблона страницы редактирования укажите шаблон создания новой записи
-    # который вы создали раньше (вы могли назвать шаблон иначе)
     profile = get_object_or_404(User, username=username)
     post = get_object_or_404(Post, id=post_id)
     if profile != request.user:
@@ -102,33 +99,3 @@ def post_view(request, username, post_id):
         'post': post,
         'author': author,
     })
-
-# @login_required
-# def post_edit(request, username, post_id):
-#     # тут тело функции. Не забудьте проверить,
-#     # что текущий пользователь — это автор записи.
-#     # В качестве шаблона страницы редактирования укажите шаблон создания новой записи
-#     # который вы создали раньше (вы могли назвать шаблон иначе)
-#     profile = get_object_or_404(User, username=username)
-#     post = get_object_or_404(Post, id=post_id)
-#     if profile == post.author:
-#         if request.method == 'POST':
-#             form = PostForm(request.POST)
-#             if form.is_valid():
-#                 edit_post = form.save(commit=False)
-#                 edit_post.author = post.author
-#                 edit_post.id = post.id
-#                 edit_post.pub_date = post.pub_date
-#                 edit_post.save()
-#                 return redirect("/")
-#     form = PostForm()
-#     context = {
-#         "form": form,
-#         "post": post,
-#         "username": username,
-#         "post_id": post_id,
-#     }
-#     return render(request, "new.html", context)
-
-
-
