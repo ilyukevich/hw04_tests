ad0cf07908978899e4acca5c4904ae3898f84c73
ilyukevich <9009901@gmail.com>
2020-08-20 23:24:45 +0300

    20/08/2020 - review3

--- posts/tests.py
+++ posts/tests.py
@@ -1,6 +1,5 @@
-# с тестами работал все ночь, поэтому думаю где-то перемудрил
-# прошу понять и простить и за переменные в том числе:)
-# постарался все ваши замечания исправить
+# благодарен за ревью всего этого)
+
 
 from django.test import TestCase, Client
 from .models import Group, Post, User
@@ -9,86 +8,99 @@ from django.urls import reverse
 
 class ProfileTest(TestCase):
     def setUp(self):
-        self.client = Client()
-        self.client2 = Client()
-        self.user = User.objects.create_user(username="sarah")
-        self.user2 = User.objects.create_user(username="anonimus")
+        self.auth_client = Client()
+        self.unauth_client = Client()
+        self.user_auth = User.objects.create_user(username="sarah")
+        self.user_unauth = User.objects.create_user(username="anonimus")
         self.group = Group.objects.create(title="titlejust", slug="just", description="description1")
-        self.client.force_login(self.user)
+        self.auth_client.force_login(self.user_auth)
 
 
-    def search(self, post):
-        """проверка наличия поста на всех нужных страницах"""
-        # проверка, что новая запись появилась на всех связанных страницах
-        value = post
+    def search_text_page(self, text):
+        """checking the presence of a post on all the necessary pages
+        (new entry appeared on all linked pages)"""
+
+        search_text = text
         for url in (
             reverse("index"),
-            reverse("profile", kwargs={"username": self.user}),
-            reverse("post", kwargs={"username": self.user, "post_id": self.post.id}),
+            reverse("profile", kwargs={"username": self.user_auth}),
+            reverse("post", kwargs={"username": self.user_auth, "post_id": self.post.id}),
             reverse("group", kwargs={"slug": self.group.slug})
         ):
-            response = self.client.get(url)
-            self.assertContains(response, value)
+            response = self.auth_client.get(url)
+            self.assertContains(response, search_text)
 
 
-    def check(self, post):
-        """проверка всех полей из контекста"""
-        post = Post.objects.get(author=self.user, id=self.post.id)
-        value = post
-        self.assertEqual(post.text, value.text)
-        self.assertEqual(post.author, value.author)
-        self.assertEqual(post.group, value.group)
+    def check_fields(self, post_text):
+        """checking all fields from context"""
+
+        post_get = Post.objects.get(author=self.user_auth, id=self.post.id)
+        value = post_text
+        self.assertEqual(post_get.text, value)
+        self.assertEqual(post_get.author, self.user_auth)
+        self.assertEqual(post_get.group, self.group)
 
 
     def test_personal_page(self):
         """After registering a user, his personal page (profile) is created"""
-        response = self.client.get(reverse("profile", args=[self.user]))
+
+        response = self.auth_client.get(reverse("profile", args=[self.user_auth]))
         self.assertEqual(response.status_code, 200)
 
-    # В тесте на публикацию поста авторизованным пользователем проверяем группу автора текст
-    # и количество постов в базе. Проверяя количество постов в базе используем метод.count().
+
     def test_create_post_auth(self):
-        """test_create_post"""
-        # An authorized user can publish a post (new)
-        posts_count_now = Post.objects.filter(author=self.user).count()
+        """test_create_post
+        An authorized user can publish a post (new),
+        check the author's group, text and number of posts in the database"""
+
+        self.text_new = "just test text"
+        posts_count_now = Post.objects.filter(author=self.user_auth).count()
         self.post = Post.objects.create(
-            text="just test text",
-            author=self.user, group=self.group)
-        posts_count_last = Post.objects.filter(author=self.user).count()
+            text=self.text_new,
+            author=self.user_auth, group=self.group)
+        posts_count_last = Post.objects.filter(author=self.user_auth).count()
         self.assertEqual(posts_count_last, posts_count_now + 1)
-        response = self.client.get(reverse("profile", args=[self.user]))
+        response = self.auth_client.get(reverse("profile", args=[self.user_auth]))
         self.assertEqual(len(response.context["author_posts"]), 1)
         self.assertContains(response, self.post.text)
         self.assertContains(response, self.post.group)
-        # проверка наличия поста на всех нужных страницах
-        value = self.post.text
-        self.search(value)
-        # проверка всех полей из контекста
-        self.check(self.post)
+
+        # check if a post is present on all required pages
+        self.search_text_page(self.text_new)
+
+        # checking all fields from context
+        self.check_fields(self.text_new)
 
 
     def test_create_post_unauth(self):
-        """test_create_post"""
-        # An unauthorized user can publish a post (new)
-        posts_count_now = Post.objects.filter(author=self.user2).count()
-        response = self.client2.post("/new/",
-            text="just test text",
-            author=self.user, group=self.group, follow=True)
-        posts_count_last = Post.objects.filter(author=self.user).count()
+        """test_create_post
+        An unauthorized user can publish a post (new)"""
+        test_text = "Python"
+        posts_count_now = Post.objects.filter(author=self.user_unauth).count()
+        response = self.unauth_client.post(reverse('new_post'),
+                                    {'text': test_text, 'author': self.user_unauth,
+                                     'group': self.group.id}, follow=True)
+        posts_count_last = Post.objects.filter(author=self.user_unauth).count()
         self.assertEqual(posts_count_last, posts_count_now)
-        self.assertRedirects(response, '/auth/login/?next=/new/')
+        self.assertRedirects(response, f"{reverse('login')}?next=/new/")
 
 
     def test_edit(self):
+        """test_edit_post
+                An authorized user can edit a post"""
+        self.text_create = "Old just test text11111"
+        self.text_edit = "New text11111"
         self.post = Post.objects.create(
-             text="just test text11111",
-             author=self.user, group=self.group)
-        response = self.client.post(
+             text=self.text_create,
+             author=self.user_auth, group=self.group)
+        post_now = Post.objects.filter(author=self.user_auth).count()
+        response = self.auth_client.post(
             reverse("post_edit",
-                    kwargs={"username": self.user,
+                    kwargs={"username": self.user_auth,
                             "post_id": self.post.id, }),
-                    {'text': 'New text'})
-        post_get = Post.objects.get(author=self.user, id=self.post.id)
+                    {'text': self.text_edit, 'group': self.group.id})
         self.assertEqual(response.status_code, 302)
-        value = post_get.text
-        self.check(response)
+        post_update = Post.objects.filter(author=self.user_auth).count()
+        self.assertEqual(post_now, post_update)
+        self.search_text_page(self.text_edit)
+        self.check_fields(self.text_edit)
